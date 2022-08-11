d7f905ea3f014f39b969069332fbb5f7cca743b6
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-11 11:20:26 -0400

    Adds more options for instruments, languages, etc

--- Options.cs
+++ Options.cs
@@ -6,18 +6,195 @@ namespace DnDCharacterCreator.Options
 {
     public enum Race
     {
+        None,
+        Dragonborn,
+        Dwarf,
+        Elf,
+        Gnome,
+        HalfElf,
+        Halfling,
+        HalfOrc,
+        Human,
+        Tiefling,
 
     }
     public enum Class
     {
-
+        None,
+        Barbarian,
+        Bard,
+        Cleric,
+        Druid,
+        Fighter,
+        Monk,
+        Paladin,
+        Ranger,
+        Rouge, 
+        Sorcerer,
+        Warlock,
+        Wizard
     }
     public enum Weapon
     {
-
+        Club,
+        Dagger,
+        Greatclub,
+        Handaxe,
+        Javelin,
+        LightHammer,
+        Mace,
+        Quarterstaff,
+        Sickle,
+        Spear,
+        Crossbow,
+        Dart,
+        Shortbow,
+        Sling,
+        Battleaxe,
+        Flail,
+        Glaive,
+        Greataxe,
+        Greatsword,
+        Halberd,
+        Lance,
+        Longsword,
+        Maul,
+        Morningstar,
+        Pike,
+        Rapier,
+        Scimitar,
+        Shortsword,
+        Trident,
+        WarPick,
+        Warhammer,
+        Whip,
+        Blowgun,
+        Longbow,
+        HeavyCrossbow,
+        Net
     }
     public enum Armor
     {
-
+        Light,
+        Medium,
+        Heavy,
+        Shield
+    }
+    public enum Ability
+    {
+        Strength,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Charisma
+    }
+    public enum Skill
+    {
+        Athletics,
+        Acrobatics,
+        SleightOfHand,
+        Stealth,
+        Arcana,
+        History,
+        Investigation,
+        Nature,
+        Religion,
+        AnimalHandling,
+        Insight,
+        Medicine,
+        Perception,
+        Survival,
+        Deception,
+        Intimidation,
+        Performance,
+        Persuasion
+    }
+    public enum Instrument
+    {
+        Bagpipes,
+        Drum,
+        Dulcimer,
+        Flute,
+        Lute,
+        Lyre,
+        Horn,
+        PanFlute,
+        Shawm,
+        Viol
+    }
+    public enum HalflingSubrace
+    {
+        Lightfoot,
+        Stout
+    }
+    public enum ElfSubrace
+    {
+        High,
+        Wood,
+        Dark
+    }
+    public enum GnomeSubrace
+    {
+        Forest,
+        Rock
+    }
+    public enum StandardLanguage
+    {
+        Common,
+        Dwarvish,
+        Elvish,
+        Giant,
+        Gnomish,
+        Gobline,
+        Halfing,
+        Orc
+    }
+    public enum ExoticLanguage
+    {
+        Abyssal,
+        Celestial,
+        Draconic,
+        DeepSpeech,
+        Infernal,
+        Primordial,
+        Sylvan,
+        Undercommon
+    }
+    public enum DamageType
+    {
+        Slashing,
+        Bludgeoning,
+        Piercing,
+        Poison,
+        Acid,
+        Fire,
+        Cold,
+        Radiant,
+        Necrotic,
+        Lightning,
+        Thunder,
+        Force,
+        Psychic
+    }
+    public enum ArtisanTool
+    {
+        AlchemistSupplies,
+        BrewerSupplies,
+        CaligrapherSupplies,
+        CarpenterTools,
+        CartographerTools,
+        CobblerTools,
+        CookUtensils,
+        GlassblowerTools,
+        JewelerTools,
+        LeatherworkTools,
+        MasonTools,
+        PainterTools,
+        PotterTools,
+        SmithTools,
+        TinkerTools,
+        WeaverTools,
+        WoodcarverTools
     }
 }
