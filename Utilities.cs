346c5b5fc63bd355ceb003d26d789a168c07d681
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-11 11:24:39 -0400

    Adds a utility class to help with random methods

---
+++ Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDCharacterCreator
+{
+    internal static class Utilities
+    {
+        public static int GetEnumLength<T>()
+        {
+            return Enum.GetNames(typeof(T)).Length;
+        }
+    }
+}
