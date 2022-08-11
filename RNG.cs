e63f1aa0f459b281a8b60a16857dce604aa60c57
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-11 11:20:57 -0400

    Adds overloads for more flexibility

--- RNG.cs
+++ RNG.cs
@@ -10,6 +10,20 @@ namespace DnDCharacterCreator
     {
         private static Random random = new Random();
 
-        
+        public static int Roll() => random.Next(1,21);
+        public static int Roll(int high) => random.Next(1, high + 1);
+        public static int Roll(int low, int high) => random.Next(low, high);
+        public static T ReturnRandom<T>(List<T> collection) => collection[random.Next(collection.Count)];
+        public static T ReturnRandom<T>(T[] collection) => collection[random.Next(collection.Length)];
+        /// <summary>
+        /// ReturnRandom type of EnumType to return random from Enum
+        /// </summary>
+        /// <typeparam name="T">Enum</typeparam>
+        /// <returns>Random Enum Value</returns>
+        public static T ReturnRandom<T>()
+        {
+            var item = Enum.GetValues(typeof(T));
+            return (T)item.GetValue(random.Next(item.Length));
+        }
     }
 }
