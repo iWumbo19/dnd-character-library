0b919b67860d13eea34f0fe3e37b958deb424175
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-11 14:39:47 -0400

    Changes Generate method after Race fix

--- Models/Character.cs
+++ Models/Character.cs
@@ -12,16 +12,14 @@ namespace DnDCharacterCreator.Models
             Resistances = new bool[Utilities.GetEnumLength<DamageType>()];
             ToolProficiency = new bool[Utilities.GetEnumLength<ArtisanTool>()];
             InstrumentProficiency = new bool[Utilities.GetEnumLength<Instrument>()];
-            StandardLanguage = new bool[Utilities.GetEnumLength<StandardLanguage>()];
-            ExoticLanguage = new bool[Utilities.GetEnumLength<ExoticLanguage>()];
+            StandardLanguages = new bool[Utilities.GetEnumLength<StandardLanguage>()];
+            ExoticLanguages = new bool[Utilities.GetEnumLength<ExoticLanguage>()];
             WeaponProficiency = new bool[Utilities.GetEnumLength<Weapon>()];
             ArmorProficiency = new bool[Utilities.GetEnumLength<Armor>()];
         }
         public Race Race { get; set; }
         public Class Class { get; set; }
-        public string FirstName { get; set; }
-        public string Nickname { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
         public string Background { get; set; }
         public Stats Stats { get; set; }
         public bool[] Resistances { get; set; }
@@ -29,16 +27,16 @@ namespace DnDCharacterCreator.Models
         public bool[] ArmorProficiency { get; set; }
         public bool[] WeaponProficiency { get; set; }
         public bool[] InstrumentProficiency { get; set; }
-        public bool[] StandardLanguage { get; set; }
-        public bool[] ExoticLanguage { get; set; }
+        public bool[] StandardLanguages { get; set; }
+        public bool[] ExoticLanguages { get; set; }
         public int MaxHealth { get; set; }
         public int CurrentHealth { get; set; }
 
         public bool IsResistant(DamageType type) => Resistances[(int)type];
         public bool IsProficient(ArtisanTool tool) => ToolProficiency[(int)tool];
         public bool IsProficient(Instrument instrument) => InstrumentProficiency[(int)instrument];
-        public bool IsProficient(StandardLanguage standard) => StandardLanguage[(int)standard];
-        public bool IsProficient(ExoticLanguage exotic) => ExoticLanguage[(int)exotic];
+        public bool IsProficient(StandardLanguage standard) => StandardLanguages[(int)standard];
+        public bool IsProficient(ExoticLanguage exotic) => ExoticLanguages[(int)exotic];
         public bool IsProficient(Weapon weapon) => WeaponProficiency[(int)weapon];
         public bool IsProficient(Armor armor) => ArmorProficiency[(int)armor];
     }
