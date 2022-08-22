89f8698677d536cc00642d276c75bc32bb7489f2
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-22 12:19:45 -0400

    Adds alignment class

---
+++ Alignment.cs
@@ -0,0 +1,18 @@
+﻿using DnDCharacterCreator.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDCharacterCreator
+{
+    public class Alignment
+    {
+        public Law Law;
+        public Order Order;
+
+        public override string ToString()
+        {
+            return $"{Law} {Order}";
+        }
+    }
+}
