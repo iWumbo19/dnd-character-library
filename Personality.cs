fb286c4d8e51e721b975f6ce6ad65bd0b7e4357c
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-22 12:19:36 -0400

    Adds new personality object

---
+++ Personality.cs
@@ -0,0 +1,29 @@
+﻿using DnDCharacterCreator.Interfaces;
+using DnDCharacterCreator.Models;
+using DnDCharacterCreator.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDCharacterCreator
+{
+    public class Personality
+    {
+        public string Trait;
+        public string Ideal;
+        public string Bond;
+        public string Flaw;
+        public Alignment Alignment;
+        public IBackground Background;
+
+        public Personality()
+        {
+           
+        }
+        
+
+
+
+
+    }
+}
