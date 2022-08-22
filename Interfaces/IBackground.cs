795c6100d59cdf95d97b012747c1548f65b401a6
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-22 12:18:27 -0400

    New Interface for implementing background

---
+++ Interfaces/IBackground.cs
@@ -0,0 +1,19 @@
+﻿using DnDCharacterCreator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDCharacterCreator.Interfaces
+{
+    public interface IBackground
+    {
+        
+        public void Build(Character character);
+
+        public string ChooseTrait();
+        public string ChooseIdeal(Character character);
+        public string ChooseBond();
+        public string ChooseFlaw();
+
+    }
+}
