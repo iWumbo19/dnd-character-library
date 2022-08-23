93abfcebbb591d15cf6ae236b5867721e018d24e
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-23 09:12:14 -0400

    Adds Entertainer BG

---
+++ Backgrounds/Entertainer.cs
@@ -0,0 +1,36 @@
+﻿using DnDCharacterCreator.Interfaces;
+using DnDCharacterCreator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDCharacterCreator.Backgrounds
+{
+    public class Entertainer : IBackground
+    {
+        public void Build(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ChooseBond()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ChooseFlaw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ChooseIdeal(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ChooseTrait()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
