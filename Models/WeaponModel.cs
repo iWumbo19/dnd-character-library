3efddfeb73735dd47a498b155530f92e08c3c80f
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-24 14:40:32 -0400

    Adds weapon factory and weapon model for equipment

---
+++ Models/WeaponModel.cs
@@ -0,0 +1,24 @@
+﻿using DnDCharacterCreator.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDCharacterCreator.Models
+{
+    public class WeaponModel
+    {
+        public int DieAmount { get; private set; }
+        public int DamageDie { get; private set; }
+        public string Name { get; private set; }
+        public DamageType DamageType { get; private set; }
+        public WeaponType WeaponType { get; private set; }
+        public WeaponModel(string name, int dieAmount, int dieDmg, DamageType type, WeaponType weapontype)
+        {
+            DieAmount = dieAmount;
+            DamageDie = dieDmg;
+            Name = name;
+            DamageType = type;
+            WeaponType = weapontype;
+        }
+    }
+}
